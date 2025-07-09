3f719ba3ac63d460012582b25cb20cc8beb23aeb
nghiadx-kyla <nghiadx@kyla.co.jp>
2025-07-09 21:06:48 +0700

    feat: add js-cookie for language management and update middleware for next-intl

--- src/components/languageSelector/languageSelector.tsx
+++ src/components/languageSelector/languageSelector.tsx
@@ -2,16 +2,20 @@
 import { useState, useEffect, useRef } from 'react';
 import { useTranslations } from 'next-intl';
 import styles from './languageSelector.module.css';
+import Cookies from 'js-cookie';
+
+const LOCALE_COOKIE_NAME = 'NEXT_LOCALE';
 const langs = [
     { code: 'vi', label: 'lang_vi' },
     { code: 'en', label: 'lang_en' }
 ];
+
 export default function LanguageSelector() {
+    const [selectedLanguage, setSelectedLanguage] = useState<string>('en'); // Always 'en' on SSR    
+    const [isMounted, setIsMounted] = useState(false); // State to track if the component has mounted
     const componentRef: any = useRef(null); // Ref to check if the click is inside or outside the component
 
     const handleClickOutside = (event: any) => {
-        // Check if the click occurred outside of this component
-        // current property of the ref is null if the component is not mounted
         if (componentRef.current && !componentRef.current.contains(event.target)) {
             // Perform actions when a click outside occurs, e.g., close a dropdown
             setIsOpen(false); // Ensure the dropdown is closed when the component mounts
@@ -19,25 +23,17 @@ export default function LanguageSelector() {
         // Perform actions when a click inside occurs
         // else {
         //   console.log('Clicked inside MyComponent or on its children!');
-
         // }
     };
 
     useEffect(() => {
+        const cookieLanguage = Cookies.get(LOCALE_COOKIE_NAME);
+        if (cookieLanguage && cookieLanguage !== selectedLanguage) {
+            setSelectedLanguage(cookieLanguage);
+        }
+        setIsMounted(true); // Set the component as mounted after the initial render
         // Add the event listener when the component mounts
         window.addEventListener('click', handleClickOutside);
-         // get language from localStorage
-        const storedLanguage = localStorage.getItem('selectedLanguage');
-        if (storedLanguage) {            
-            setSelectedLanguage(storedLanguage);
-        } else {
-            // If no language is stored, default to English
-            // Set the initial language based on the browser's language setting
-            // const browserLanguage = navigator.language;
-            // const langCode = browserLanguage.startsWith('vi') ? 'vi' : 'en';            
-            // setSelectedLanguage(langCode);
-        }
-
         // Clean up the event listener when the component unmounts
         // This is crucial to prevent memory leaks        
         return () => {
@@ -50,33 +46,31 @@ export default function LanguageSelector() {
     const toggleLanguageList = () => {
         setIsOpen(!isOpen);
     }
-    const [selectedLanguage, setSelectedLanguage] = useState(''); // Default to English
+
     const handleLanguageChange = (lang: string) => {
         if (lang === selectedLanguage) {
             setIsOpen(false); // Close the dropdown if the same language is selected
             return; // If the selected language is already active, do nothing
         }
-        setSelectedLanguage(lang);
-        localStorage.setItem('selectedLanguage', lang);
+        // setSelectedLanguage(lang);
         // change to same route with new language
-        const currentPath = window.location.pathname;        
-        const langPrefix = currentPath.split('/'); 
+        const currentPath = window.location.pathname;
+        const langPrefix = currentPath.split('/');
         langPrefix[1] = lang; // Update the language prefix in the path
         // Join the parts back into a path string
-        const newPath = langPrefix.join('/');        
+        const newPath = langPrefix.join('/');
         // Update the URL without reloading the page
         window.history.pushState({}, '', newPath);
-        setIsOpen(false); // Close the dropdown after changing the language        
+        // setIsOpen(false); // Close the dropdown after changing the language        
         window.location.reload(); // Reload the page to apply the new language
-    }    
-
+    }
 
     return (
         <div className={`${styles.languageSelector}`} ref={componentRef}>
-            <button className={`${styles.languageButton} ${styles[selectedLanguage]}`} onClick={toggleLanguageList}></button>
+            <button className={`${styles.languageButton} ${isMounted ? styles[selectedLanguage] : 'bg-gray-100 rounded-xl'}`} onClick={toggleLanguageList}></button>
             <ul className={styles.languageList + (isOpen ? ' flex' : ' hidden')}>
                 {langs.map(lang => (
-                    <li key={lang.code} className={styles.languageItem} onClick={() => { handleLanguageChange(lang.code)}}>
+                    <li key={lang.code} className={styles.languageItem} onClick={() => { handleLanguageChange(lang.code) }}>
                         <button className={`${styles.languageButton} ${styles[lang.code]}`}></button>
                         <span className={styles.languageLabel}>{t(lang.label)}</span>
                     </li>
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,12 @@
-import createMiddleware from 'next-intl/middleware';
+import createNextIntlMiddleware from 'next-intl/middleware';
 import {routing} from './i18n/routing';
+import { NextRequest } from 'next/server';
  
-export default createMiddleware(routing);
+const nextIntlMiddleware = createNextIntlMiddleware(routing)
+
+export async function middleware(request: NextRequest) {    
+  return nextIntlMiddleware(request);
+}
  
 export const config = {
   // Match all pathnames except for
