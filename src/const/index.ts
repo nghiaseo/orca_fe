16d3e29b5164141c03293d392936af0613321638
nghiadx-kyla <nghiadx@kyla.co.jp>
2025-07-09 21:17:39 +0700

    feat: update course items and descriptions for improved clarity and add pronunciation course

--- src/const/index.ts
+++ src/const/index.ts
@@ -1,39 +1,48 @@
 export const APP_TITLE = "Orca Edu";
-export const COURSE_ITEMS:MenuItem[] = [
-            {
-                key:'1-1',
-                label:'ielts',
-                isActive: false,
-                disabled: true,
-                description: 'ielts_description',
-                backgroundColor:'--primary',
-            },
-            {
-                key:'1-2',
-                label:'toeic',
-                isActive: false,
-                disabled: true,
-                description: 'toeic_description',
-                backgroundColor:'--success',
-            },
-            {
-                key:'1-3',
-                label:'pte',
-                isActive: false,
-                disabled: false,
-                description: 'pte_description',
-                backgroundColor:'--info',
-            },
-            {
-                key:'1-4',
-                label:'cll',
-                isActive: false,
-                disabled: true,
-                description: 'cll_description',
-                backgroundColor:'--ai-purple',
-            },
-        ]
-export const MENU_ITEMS:MenuItem[] = [
+export const COURSE_ITEMS: MenuItem[] = [
+    {
+        key: '1-1',
+        label: 'pte',
+        isActive: false,
+        disabled: false,
+        description: 'pte_description',
+        backgroundColor: '--info',
+    },
+    {
+        key: '1-2',
+        label: 'pronunciation',
+        isActive: false,
+        disabled: false,
+        description: 'pronunciation_description',
+        backgroundColor: '--secondary',
+    },
+    {
+        key: '1-3',
+        label: 'ielts',
+        isActive: false,
+        disabled: true,
+        description: 'ielts_description',
+        backgroundColor: '--primary',
+    },
+    {
+        key: '1-4',
+        label: 'toeic',
+        isActive: false,
+        disabled: true,
+        description: 'toeic_description',
+        backgroundColor: '--success',
+    },
+
+    {
+        key: '1-5',
+        label: 'cll',
+        isActive: false,
+        disabled: true,
+        description: 'cll_description',
+        backgroundColor: '--ai-purple',
+    },
+]
+export const MENU_ITEMS: MenuItem[] = [
     {
         key: '1',
         label: 'course',
@@ -45,7 +54,7 @@ export const MENU_ITEMS:MenuItem[] = [
         key: '2',
         label: 'test_input',
         isActive: false,
-        disabled: true,        
+        disabled: true,
     },
     {
         key: '3',
